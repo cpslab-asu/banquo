6cc641183000d720659e5306a5aded2d7b20c047
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-12-14 14:10:57 -0700

    Define basic combinators in common module

--- src/parser/common.rs
+++ src/parser/common.rs
@@ -1,7 +1,11 @@
-
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
+
+use nom::{IResult, Parser};
+use nom::bytes::complete::tag;
+use nom::character::complete::{alpha1, digit0, space0};
+use nom::sequence::{delimited, pair};
 
 use crate::formula::{Formula, HybridDistanceFormula};
 use crate::trace::Trace;
@@ -78,3 +82,29 @@ impl<'a> Display for IncompleteParseError<'a> {
 }
 
 impl<'a> Error for IncompleteParseError<'a> {}
+
+pub fn var_name(input: &str) -> IResult<&str, String> {
+    let mut parser = pair(alpha1, digit0);
+    let (rest, (s1, s2)) = parser(input)?;
+    let name = s1.to_string() + s2;
+
+    Ok((rest, name))
+}
+
+pub fn op0<'a>(op: &'a str) -> impl FnMut(&'a str) -> IResult<&'a str, &'a str> {
+    move |input: &'a str| -> IResult<&'a str, &'a str> {
+        let mut parser = delimited(space0, tag(op), space0);
+        parser(input)
+    }
+}
+
+pub fn subformula<'a, F, T>(subparser: F) -> impl FnMut(&'a str) -> IResult<&'a str, T>
+where
+    F: Parser<&'a str, T, nom::error::Error<&'a str>>    
+{
+    let mut parser = delimited(tag("("), subparser, tag(")"));
+
+    move |input: &str| {
+        parser.parse(input)
+    }
+}
