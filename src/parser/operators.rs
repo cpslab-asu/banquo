4f2d1f0080e004793cebb9d903e934ccb662aea6
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-12-19 15:25:41 -0700

    Update temporal operator parser to parse time bounds as floats instead of integers

--- src/parser/operators.rs
+++ src/parser/operators.rs
@@ -3,8 +3,8 @@ use std::str::FromStr;
 use nom::branch::{alt, Alt};
 use nom::bytes::complete::tag;
 use nom::character::complete::digit1;
-use nom::combinator::{map_res, opt};
-use nom::sequence::{preceded, tuple};
+use nom::combinator::{map_res, opt, recognize};
+use nom::sequence::{pair, preceded, tuple};
 use nom::{IResult, Parser};
 
 use crate::operators::{Always, And, Eventually, Implies, Next, Not, Or};
@@ -90,13 +90,21 @@ where
     unaryop((tag("X"), tag("()"), tag("next")), subparser, Next::new)
 }
 
-fn integer(input: &str) -> IResult<&str, usize> {
-    let mut parser = map_res(digit1, usize::from_str);
+fn number(input: &str) -> IResult<&str, f64> {
+    let make_number = |(front, back): (&str, Option<&str>)| {
+        let num_str = front.to_string() + back.unwrap_or("");
+        f64::from_str(&num_str)
+    };
+
+    let back_parser = pair(tag("."), digit1);
+    let num_parser = pair(digit1, opt(recognize(back_parser)));
+    let mut parser = map_res(num_parser, make_number);
+
     parser(input)
 }
 
-fn time_bounds(input: &str) -> IResult<&str, (usize, usize)> {
-    let mut parser = tuple((tag("{"), integer, tag(","), integer, tag("}")));
+fn time_bounds(input: &str) -> IResult<&str, (f64, f64)> {
+    let mut parser = tuple((tag("{"), number, tag(","), number, tag("}")));
     let (rest, (_, t_start, _, t_end, _)) = parser(input)?;
 
     Ok((rest, (t_start, t_end)))
@@ -106,7 +114,7 @@ fn boundedop<'a, O, S, T, F, U>(ops: O, mut subparser: S, func: F) -> impl FnMut
 where
     O: Alt<&'a str, &'a str, nom::error::Error<&'a str>>,
     S: Parser<&'a str, T, nom::error::Error<&'a str>>,
-    F: Fn(T, Option<(usize, usize)>) -> U,
+    F: Fn(T, Option<(f64, f64)>) -> U,
 {
     let mut bounds = preceded(alt(ops), opt(time_bounds));
 
@@ -122,12 +130,26 @@ pub fn always<'a, S, T>(subparser: S) -> impl FnMut(&'a str) -> IResult<&'a str,
 where
     S: Parser<&'a str, T, nom::error::Error<&'a str>>,
 {
-    boundedop((tag("always"), tag("[]"), tag("G")), subparser, Always::new)
+    let ctor = |subformula, opt_t_bounds| -> Always<T> {
+        match opt_t_bounds {
+            Some(t_bounds) => Always::new_bounded(subformula, t_bounds),
+            None => Always::new_unbounded(subformula),
+        }
+    };
+
+    boundedop((tag("always"), tag("[]"), tag("G")), subparser, ctor)
 }
 
 pub fn eventually<'a, S, T>(subparser: S) -> impl FnMut(&'a str) -> IResult<&'a str, Eventually<T>>
 where
     S: Parser<&'a str, T, nom::error::Error<&'a str>>,
 {
-    boundedop((tag("eventually"), tag("<>"), tag("F")), subparser, Eventually::new)
+    let ctor = |subformula, opt_t_bounds| -> Eventually<T> {
+        match opt_t_bounds {
+            Some(t_bounds) => Eventually::new_bounded(subformula, t_bounds),
+            None => Eventually::new_unbounded(subformula),
+        }
+    };
+
+    boundedop((tag("eventually"), tag("<>"), tag("F")), subparser, ctor)
 }
