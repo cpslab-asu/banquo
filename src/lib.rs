d1a6219a6cd962c18f3f7ec3e98e4805461a61e8
Quinn Thibeault <quinn.thibeault96@gmail.com>
2022-12-27 16:03:25 -0700

    Update data structures exposed at package top level

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,4 @@ pub mod parser;
 pub mod trace;
 pub mod formulas;
 
-pub use crate::formula::{Formula, HybridDistance, HybridDistanceFormula, Result};
-pub use crate::trace::{States, Times, Trace};
+pub use crate::trace::Trace;
