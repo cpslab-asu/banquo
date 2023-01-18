bf4cbb207bccc8f591dae4e723a4dd4a6b72f5c7
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-01-18 09:08:09 -0700

    Create formula trait that is generic over the type of its output. Define an associated state type so that types can only implement the formula trait for a single input/output pair.

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,15 @@ pub mod parser;
 pub mod trace;
 
 pub use crate::trace::Trace;
+
+/// Evaluate a trace of states into their associated output values
+pub trait Formula<Output> {
+    /// The type of state the formula can evaluate
+    type State;
+
+    /// The type of error that can be generated during evaluation
+    type Error;
+
+    /// Evaluate each state in a trace into an output value
+    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error>;
+}
