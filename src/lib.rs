61acb1f8b21d62c911c31d7825b029f85a2c5c94
Quinn Thibeault <qthibeau@asu.edu>
2023-01-19 11:42:55 -0700

    Implement Formula trait for smart pointer types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![deny(clippy::all)]
 
+use std::rc::Rc;
+use std::sync::Arc;
+
 pub mod automaton;
 pub mod expressions;
 pub mod formulas;
@@ -15,8 +18,60 @@ pub trait Formula<Output> {
     type State;
 
     /// The type of error that can be generated during evaluation
-    type Error;
+    type Error: std::error::Error;
 
     /// Evaluate each state in a trace into an output value
     fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error>;
+}
+
+impl<Output, T> Formula<Output> for &T
+where
+    T: Formula<Output> + ?Sized,
+{
+    type State = T::State;
+    type Error = T::Error;
+
+    #[inline]
+    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
+        (**self).evaluate_states(trace)
+    }
+}
+
+impl<Output, T> Formula<Output> for Box<T>
+where
+    T: Formula<Output> + ?Sized,
+{
+    type State = T::State;
+    type Error = T::Error;
+
+    #[inline]
+    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
+        (**self).evaluate_states(trace)
+    }
+}
+
+impl<Output, T> Formula<Output> for Rc<T>
+where
+    T: Formula<Output> + ?Sized,
+{
+    type State = T::State;
+    type Error = T::Error;
+
+    #[inline]
+    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
+        (**self).evaluate_states(trace)
+    }
+}
+
+impl<Output, T> Formula<Output> for Arc<T>
+where
+    T: Formula<Output> + ?Sized,
+{
+    type State = T::State;
+    type Error = T::Error;
+
+    #[inline]
+    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
+        (**self).evaluate_states(trace)
+    }
 }
