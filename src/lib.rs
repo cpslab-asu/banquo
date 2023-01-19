1b6714048d2049825e66fff2d6bcc40d67f4b7d9
Quinn Thibeault <qthibeau@asu.edu>
2023-01-19 13:12:31 -0700

    Create helper methods to evaluate specific types of formulas

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![deny(clippy::all)]
 
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::rc::Rc;
 use std::sync::Arc;
 
@@ -9,6 +11,8 @@ pub mod formulas;
 pub mod operators;
 pub mod parser;
 pub mod trace;
+
+use formulas::HybridDistance;
 
 pub use crate::trace::Trace;
 
@@ -74,4 +78,49 @@ where
     fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
         (**self).evaluate_states(trace)
     }
+}
+
+#[derive(Debug)]
+pub enum EvaluationError<'a> {
+    EmptyTrace,
+    FormulaError(Box<dyn Error + 'a>),
+}
+
+impl<'a> Display for EvaluationError<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::EmptyTrace => write!(f, "Cannot evaluate an empty trace"),
+            Self::FormulaError(err) => Display::fmt(err, f),
+        }
+    }
+}
+
+impl<'a> Error for EvaluationError<'a> {}
+
+pub fn eval_robustness<'a, F>(formula: F, trace: impl AsRef<Trace<F::State>>) -> Result<f64, EvaluationError<'a>>
+where
+    F: Formula<f64>,
+    F::Error: 'a,
+{
+    let robustness_trace = formula.evaluate_states(trace.as_ref()).map_err(|err| EvaluationError::FormulaError(Box::new(err)))?;
+    let (_, rho) = robustness_trace
+        .into_iter()
+        .next()
+        .ok_or_else(|| EvaluationError::EmptyTrace)?;
+
+    Ok(rho)
+}
+
+pub fn eval_hybrid_distance<'a, F>(formula: F, trace: impl AsRef<Trace<F::State>>) -> Result<HybridDistance, EvaluationError<'a>>
+where
+    F: Formula<HybridDistance>,
+    F::Error: 'a,
+{
+    let distance_trace = formula.evaluate_states(trace.as_ref()).map_err(|err| EvaluationError::FormulaError(Box::new(err)))?;
+    let (_, distance) = distance_trace
+        .into_iter()
+        .next()
+        .ok_or_else(|| EvaluationError::EmptyTrace)?;
+
+    Ok(distance)
 }
