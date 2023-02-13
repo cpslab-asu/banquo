76742e92e9d61fa59d57da4ef26d368014567152
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-13 08:27:21 -0700

    Refactor until operator to use updated Formula trait

--- src/operators/backwards.rs
+++ src/operators/backwards.rs
@@ -1,107 +1,56 @@
 use super::binary::BinaryOperatorError;
-use crate::formulas::HybridDistance;
+use crate::formulas::Formula;
+use crate::metric::{Bottom, Join, Meet, Top};
 use crate::trace::Trace;
-use crate::Formula;
 
-pub struct Until<L, R> {
-    left: L,
-    right: R,
+pub struct Until<Left, Right> {
+    left: Left,
+    right: Right,
 }
 
-impl<L, R> Until<L, R> {
-    pub fn new(left: L, right: R) -> Self {
+impl<Left, Right> Until<Left, Right> {
+    pub fn new(left: Left, right: Right) -> Self {
         Self { left, right }
     }
 }
 
-fn until<T, F, G>(
-    left_trace: Trace<T>,
-    right_trace: Trace<T>,
-    trace_initial: T,
-    subtrace_initial: T,
-    f: F,
-    g: G,
-) -> Trace<T>
+impl<Left, Right, State, Metric> Formula<State> for Until<Left, Right>
 where
-    T: Copy,
-    F: Fn(T, T) -> T,
-    G: Fn(T, T) -> T,
+    Left: Formula<State, Metric = Metric>,
+    Right: Formula<State, Metric = Metric>,
+    Metric: Top + Bottom + for<'a> Meet<&'a Metric> + Meet + for<'a> Join<&'a Metric>,
 {
-    let mut elements = Vec::new();
-    let mut last_rob = trace_initial;
-
-    for (time, right_rob) in right_trace.into_iter().rev() {
-        let left_rob = left_trace
-            .range(..=time)
-            .into_iter()
-            .fold(subtrace_initial.clone(), |m, (_, &r)| f(m, r));
-
-        let time_rob = f(left_rob, right_rob);
-
-        last_rob = g(time_rob, last_rob);
-        elements.push((time, last_rob));
-    }
-
-    Trace::from_iter(elements)
-}
-
-impl<Left, Right, State> Formula<f64> for Until<Left, Right>
-where
-    Left: Formula<f64, State = State>,
-    Right: Formula<f64, State = State>,
-{
-    type State = State;
+    type Metric = Metric;
     type Error = BinaryOperatorError<Left::Error, Right::Error>;
 
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<f64>, Self::Error> {
+    fn evaluate_trace(&self, trace: &Trace<State>) -> Result<Trace<Self::Metric>, Self::Error> {
         let left_trace = self
             .left
-            .evaluate_states(trace)
+            .evaluate_trace(trace)
             .map_err(BinaryOperatorError::LeftError)?;
+
         let right_trace = self
             .right
-            .evaluate_states(trace)
+            .evaluate_trace(trace)
             .map_err(BinaryOperatorError::RightError)?;
-        let trace = until(
-            left_trace,
-            right_trace,
-            f64::NEG_INFINITY,
-            f64::INFINITY,
-            f64::min,
-            f64::max,
-        );
 
-        Ok(trace)
-    }
-}
+        let mut evaluated_trace = Trace::default();
+        let mut last_metric = Metric::bottom();
 
-impl<Left, Right, State> Formula<HybridDistance> for Until<Left, Right>
-where
-    Left: Formula<HybridDistance, State = State>,
-    Right: Formula<HybridDistance, State = State>,
-{
-    type State = State;
-    type Error = BinaryOperatorError<Left::Error, Right::Error>;
+        for (time, right_metric) in right_trace.into_iter().rev() {
+            let left_metric = left_trace
+                .range(..=time)
+                .into_iter()
+                .fold(Metric::top(), |l, (_, r)| l.meet(r));
 
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<HybridDistance>, Self::Error> {
-        let left_trace = self
-            .left
-            .evaluate_states(trace)
-            .map_err(BinaryOperatorError::LeftError)?;
-        let right_trace = self
-            .right
-            .evaluate_states(trace)
-            .map_err(BinaryOperatorError::RightError)?;
-        let trace = until(
-            left_trace,
-            right_trace,
-            HybridDistance::Robustness(f64::INFINITY),
-            HybridDistance::Infinite,
-            HybridDistance::min,
-            HybridDistance::max,
-        );
+            let combined_metric = left_metric.meet(right_metric);
+            let next_metric = combined_metric.join(&last_metric);
 
-        Ok(trace)
+            evaluated_trace.insert_state(time, last_metric);
+            last_metric = next_metric;
+        }
+
+        Ok(evaluated_trace)
     }
 }
 
