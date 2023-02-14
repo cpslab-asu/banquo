3faf329a08e67280914e9292462aaa4302cf308e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-14 11:13:15 -0700

    Remove unused Expression trait

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,12 +1,6 @@
 use std::collections::HashMap;
-use std::error::Error;
 
 type VariableMap = HashMap<String, f64>;
-
-pub trait Expression {
-    type Error: Error;
-    fn evaluate_state(&self, variable_map: &VariableMap) -> Result<f64, Self::Error>;
-}
 
 mod hybrid_predicate;
 mod polynomial;
