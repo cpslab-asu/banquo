c748a62e77a26aa2ef857969c5155636d938b793
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-14 11:14:31 -0700

    Replace VariableMap type with re-export

--- src/expressions.rs
+++ src/expressions.rs
@@ -1,11 +1,7 @@
-use std::collections::HashMap;
-
-type VariableMap = HashMap<String, f64>;
-
 mod hybrid_predicate;
 mod polynomial;
 mod predicate;
 
 pub use hybrid_predicate::HybridPredicate;
-pub use polynomial::{Polynomial, PolynomialError, Term};
+pub use polynomial::{Polynomial, PolynomialError, Term, VariableMap};
 pub use predicate::{Predicate, PredicateError};
