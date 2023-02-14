c35e3c0ddee8c19a53a60a52a7f3ac105b24eb7a
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-14 16:48:43 -0700

    Fix VarMap trait export

--- src/expressions.rs
+++ src/expressions.rs
@@ -3,5 +3,5 @@ mod polynomial;
 mod predicate;
 
 pub use hybrid_predicate::HybridPredicate;
-pub use polynomial::{Polynomial, PolynomialError, Term, VariableMap};
+pub use polynomial::{Polynomial, PolynomialError, Term, VarMap};
 pub use predicate::{Predicate, PredicateError};
