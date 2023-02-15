8a89faac2b82ac871ff32cec9ef4da385e00532c
Quinn Thibeault <qthibeau@asu.edu>
2023-02-15 12:10:42 -0700

    Remove unused Formula trait implementation from crate root

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #![deny(clippy::all)]
 
-use std::error::Error;
-use std::fmt::{Display, Formatter};
-use std::rc::Rc;
-use std::sync::Arc;
-
 pub mod automaton;
 pub mod expressions;
 pub mod formulas;
@@ -15,73 +10,12 @@ pub mod trace;
 #[cfg(feature = "parser")]
 pub mod parser;
 
-use formulas::HybridDistance;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
-pub use crate::trace::Trace;
-
-/// Evaluate a trace of states into their associated output values
-pub trait Formula<Output> {
-    /// The type of state the formula can evaluate
-    type State;
-
-    /// The type of error that can be generated during evaluation
-    type Error: std::error::Error;
-
-    /// Evaluate each state in a trace into an output value
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error>;
-}
-
-impl<Output, T> Formula<Output> for &T
-where
-    T: Formula<Output> + ?Sized,
-{
-    type State = T::State;
-    type Error = T::Error;
-
-    #[inline]
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
-        (**self).evaluate_states(trace)
-    }
-}
-
-impl<Output, T> Formula<Output> for Box<T>
-where
-    T: Formula<Output> + ?Sized,
-{
-    type State = T::State;
-    type Error = T::Error;
-
-    #[inline]
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
-        (**self).evaluate_states(trace)
-    }
-}
-
-impl<Output, T> Formula<Output> for Rc<T>
-where
-    T: Formula<Output> + ?Sized,
-{
-    type State = T::State;
-    type Error = T::Error;
-
-    #[inline]
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
-        (**self).evaluate_states(trace)
-    }
-}
-
-impl<Output, T> Formula<Output> for Arc<T>
-where
-    T: Formula<Output> + ?Sized,
-{
-    type State = T::State;
-    type Error = T::Error;
-
-    #[inline]
-    fn evaluate_states(&self, trace: &Trace<Self::State>) -> Result<Trace<Output>, Self::Error> {
-        (**self).evaluate_states(trace)
-    }
-}
+use formulas::Formula;
+use metric::HybridDistance;
+use trace::Trace;
 
 #[derive(Debug)]
 pub enum EvaluationError<'a> {
@@ -100,9 +34,11 @@ impl<'a> Display for EvaluationError<'a> {
 
 impl<'a> Error for EvaluationError<'a> {}
 
-pub fn eval_robustness<'a, F>(formula: F, trace: impl AsRef<Trace<F::State>>) -> Result<f64, EvaluationError<'a>>
+type EvalResult<'a, M> = Result<M, EvaluationError<'a>>;
+
+pub fn eval_robustness<'a, F, S>(formula: F, trace: impl AsRef<Trace<S>>) -> EvalResult<'a, f64>
 where
-    F: Formula<f64>,
+    F: Formula<S, Metric = f64>,
     F::Error: 'a,
 {
     let robustness_trace = formula
@@ -116,12 +52,9 @@ where
     Ok(rho)
 }
 
-pub fn eval_hybrid_distance<'a, F>(
-    formula: F,
-    trace: impl AsRef<Trace<F::State>>,
-) -> Result<HybridDistance, EvaluationError<'a>>
+pub fn eval_hybrid_distance<'a, F, S>(formula: F, trace: impl AsRef<Trace<S>>) -> EvalResult<'a, HybridDistance>
 where
-    F: Formula<HybridDistance>,
+    F: Formula<S, Metric = HybridDistance>,
     F::Error: 'a,
 {
     let distance_trace = formula
