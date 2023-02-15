eabe6f61d96a75673cf305c8848b42413af20a9e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-14 17:50:07 -0700

    Add HybridState to expressions module re-exports

--- src/expressions.rs
+++ src/expressions.rs
@@ -2,6 +2,6 @@ mod hybrid_predicate;
 mod polynomial;
 mod predicate;
 
-pub use hybrid_predicate::HybridPredicate;
+pub use hybrid_predicate::{HybridPredicate, HybridState};
 pub use polynomial::{Polynomial, PolynomialError, Term, VarMap};
 pub use predicate::{Predicate, PredicateError};
