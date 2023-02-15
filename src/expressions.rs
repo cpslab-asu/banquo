22d6ec45c488349ed6facfe4cf3aa9aef2efc40c
Quinn Thibeault <qthibeau@asu.edu>
2023-02-15 10:13:20 -0700

    Replace VarMap export with Variables

--- src/expressions.rs
+++ src/expressions.rs
@@ -3,5 +3,5 @@ mod polynomial;
 mod predicate;
 
 pub use hybrid_predicate::{HybridPredicate, HybridState};
-pub use polynomial::{Polynomial, PolynomialError, Term, VarMap};
+pub use polynomial::{Polynomial, PolynomialError, Term, Variables};
 pub use predicate::{Predicate, PredicateError};
