0f15b118f8a50dcc524d8de59aaa59ab26f02ce3
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-02-16 08:59:51 -0700

    Export Automaton and Guard types from expressions module

--- src/expressions.rs
+++ src/expressions.rs
@@ -2,6 +2,6 @@ mod hybrid_predicate;
 mod polynomial;
 mod predicate;
 
-pub use hybrid_predicate::{HybridPredicate, HybridState};
+pub use hybrid_predicate::{Automaton, Guard, HybridPredicate, HybridState};
 pub use polynomial::{Polynomial, PolynomialError, Term, Variables};
 pub use predicate::{Predicate, PredicateError};
