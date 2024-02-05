9cfbc6434de760dee26e5e072ff30ae18e337746
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-02-05 15:09:31 -0700

    refactor: Move hybrid distance modules into feature crate

--- src/expressions.rs
+++
@@ -1,7 +0,0 @@
-mod hybrid_predicate;
-mod polynomial;
-mod predicate;
-
-pub use hybrid_predicate::{Automaton, Guard, HybridPredicate, HybridState};
-pub use polynomial::{Polynomial, PolynomialError, Term, Variables};
-pub use predicate::{Predicate, PredicateError};
