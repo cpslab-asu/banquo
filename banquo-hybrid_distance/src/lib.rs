283537097055053b5e03aed93c0695936ed01db1
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-02-06 09:21:16 -0700

    chore: Remove default lib function

--- banquo-hybrid_distance/src/lib.rs
+++ banquo-hybrid_distance/src/lib.rs
@@ -1,17 +1,2 @@
-mod predicate;
 mod automaton;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
