ac2b55af67600bad4e7dc90a2ecd426762899e8c
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-09-25 09:51:42 -0700

    Define Not operator wrapper

--- banquo-python/banquo/operators.py
+++ banquo-python/banquo/operators.py
@@ -12,8 +12,15 @@ M = typing.TypeVar("M")
 
 
 class OperatorMixin:
+    def not_(self: Formula[S, _impl.M_neg]) -> Not[S, _impl.M_neg]:
+        return Not(self)
+
     def and_(self: Formula[S, _impl.M_lt], other: Formula[S, _impl.M_lt]) -> And[S, _impl.M_lt]:
         return And(self, other)
+
+
+class Not(_impl.Not[S, _impl.M_neg], OperatorMixin):
+    ...
 
 
 class And(_impl.And[S, _impl.M_lt], OperatorMixin):
