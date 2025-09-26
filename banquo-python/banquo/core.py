c730833519c429066a8fa99ebbb341af16555480
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-09-26 13:49:21 -0700

    Remove Trace extension class from core module

--- banquo-python/banquo/core.py
+++ banquo-python/banquo/core.py
@@ -1,38 +1,12 @@
 from __future__ import annotations
 
-from collections.abc import Iterable, Mapping
-from typing import Protocol, TypeVar, cast, overload
+from typing import Protocol, TypeVar
 
-from ._banquo_impl import Trace as _Trace
+from ._banquo_impl import Trace
 
 S = TypeVar("S", contravariant=True)
 M = TypeVar("M", covariant=True)
 
 
-class Trace(_Trace[M]):
-    @overload
-    def __init__(self, elements: Mapping[float, M], /) -> None:
-        ...
-
-    @overload
-    def __init__(self, *, times: Iterable[float], states: Iterable[M]) -> None:
-        ...
-
-    def __init__(
-        self,
-        times: Mapping[float, M] | Iterable[float],
-        states: Iterable[M] | None = None,
-    ):
-        if states is not None:
-            elements = {time: state for time, state in zip(times, states)}
-        elif isinstance(times, Mapping):
-            elements = dict(cast(Mapping[float, M], times))
-        else:
-            raise ValueError("Must provide elements as two lists, or a mapping")
-
-        super().__init__(elements)
-
-
 class Formula(Protocol[S, M]):
-    def evaluate(self, trace: Trace[S]) -> Trace[M]:
-        ...
+    def evaluate(self, trace: Trace[S]) -> Trace[M]: ...
