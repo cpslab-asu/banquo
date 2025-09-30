ad7e4554def41daeb3783f0b21f566bdf843da99
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-09-30 08:36:02 -0700

    Create module to define wrapper to convert Python values into a metric

---
+++ banquo-python/src/metric.rs
@@ -0,0 +1,125 @@
+use std::cmp::Ordering;
+
+use pyo3::types::{PyAny, PyAnyMethods};
+use pyo3::{Bound, IntoPyObject, IntoPyObjectExt, Py, PyResult, Python, pyclass, pymethods};
+
+use banquo_core::{Bottom, Join, Meet, Top};
+
+// This class is a special value that represents the maximum of ALL python values.
+// Thus, this the greater-than implementation for this class will always return true.
+#[pyclass(name = "Top")]
+pub struct PyTop;
+
+#[pymethods]
+impl PyTop {
+    fn __gt__(&self, other: &Bound<'_, PyAny>) -> bool {
+        true
+    }
+}
+
+// This class is a special value that represents the minimum of ALL python values.
+// Thus, this the less-than implementation for this class will always return true.
+#[pyclass(name = "Bottom")]
+pub struct PyBottom;
+
+#[pymethods]
+impl PyBottom {
+    fn __lt__(&self, other: &Bound<'_, PyAny>) -> bool {
+        true
+    }
+}
+
+#[derive(IntoPyObject)]
+pub struct PyMetric(Py<PyAny>);
+
+impl PyMetric {
+    pub fn try_from_f64(value: f64, py: Python<'_>) -> PyResult<Self> {
+        value.into_py_any(py).map(Self)
+    }
+}
+
+impl From<Bound<'_, PyAny>> for PyMetric {
+    fn from(value: Bound<'_, PyAny>) -> Self {
+        Self(value.unbind())
+    }
+}
+
+impl Into<Py<PyAny>> for PyMetric {
+    fn into(self) -> Py<PyAny> {
+        self.0
+    }
+}
+
+// Required for PartialOrd implementation
+impl PartialEq for PyMetric {
+    fn eq(&self, other: &Self) -> bool {
+        // If equality is not defined, then the two objects are never equal
+        Python::attach(|py| self.0.bind(py).eq(&other.0).expect("Metric must support __eq__ method"))
+    }
+}
+
+fn pycmp<'py>(lhs: &Bound<'py, PyAny>, rhs: &Bound<'py, PyAny>) -> Option<Ordering> {
+    if let Ok(true) = lhs.lt(rhs) {
+        return Some(std::cmp::Ordering::Less);
+    }
+
+    if let Ok(true) = lhs.eq(rhs) {
+        return Some(std::cmp::Ordering::Equal);
+    }
+
+    if let Ok(true) = lhs.gt(rhs) {
+        return Some(std::cmp::Ordering::Greater);
+    }
+
+    // Python values cannot be compared
+    None
+}
+
+// Required for Meet implementation
+impl PartialOrd for PyMetric {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Python::attach(|py| pycmp(self.0.bind(py), other.0.bind(py)).into())
+    }
+}
+
+fn pymeet<'py>(lhs: &Bound<'py, PyAny>, rhs: &Bound<'py, PyAny>) -> Bound<'py, PyAny> {
+    let ordering = pycmp(lhs, rhs).expect("Metric must support comparison");
+    match ordering {
+        Ordering::Less | Ordering::Equal => lhs.clone(),
+        Ordering::Greater => rhs.clone(),
+    }
+}
+
+// Required for operator implementations (And, Iff, etc.)
+impl Meet for PyMetric {
+    fn min(&self, other: &Self) -> Self {
+        Python::attach(|py| pymeet(self.0.bind(py), other.0.bind(py)).into())
+    }
+}
+
+fn pyjoin<'py>(lhs: &Bound<'py, PyAny>, rhs: &Bound<'py, PyAny>) -> Bound<'py, PyAny> {
+    let ordering = pycmp(lhs, rhs).expect("Metric must support comparison");
+    match ordering {
+        Ordering::Greater | Ordering::Equal => lhs.clone(),
+        Ordering::Less => rhs.clone(),
+    }
+}
+
+// Required for operator implementations (Or, Implies, etc.)
+impl Join for PyMetric {
+    fn max(&self, other: &Self) -> Self {
+        Python::attach(|py| pyjoin(self.0.bind(py), other.0.bind(py)).into())
+    }
+}
+
+impl Top for PyMetric {
+    fn top() -> Self {
+        Python::attach(|py| PyMetric(PyBottom.into_py_any(py).unwrap()))
+    }
+}
+
+impl Bottom for PyMetric {
+    fn bottom() -> Self {
+        Python::attach(|py| PyMetric(PyBottom.into_py_any(py).unwrap()))
+    }
+}
