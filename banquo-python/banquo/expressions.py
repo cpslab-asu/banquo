3cb8557f9d2495137b3d4b679b92d588601d547b
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-10-03 11:19:35 -0700

    Refactor Predicate to inherit from TraceWrapper

--- banquo-python/banquo/expressions.py
+++ banquo-python/banquo/expressions.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
-import typing
-
-if typing.TYPE_CHECKING:
-    from collections.abc import Mapping
+from collections.abc import Mapping
 
 from ._banquo_impl import Predicate as _Predicate
 from .operators import OperatorMixin
+from .trace import TraceWrapper
 
 
-class Predicate(_Predicate, OperatorMixin):
-    def __new__(cls, coefficients: Mapping[str, float], constant: float):
-        return super().__new__(cls, dict(coefficients), constant)
+class Predicate(TraceWrapper[dict[str, float], float], OperatorMixin):
+    def __init__(self, coefficients: Mapping[str, float], constant: float):
+        super().__init__(_Predicate(dict(coefficients), constant))
