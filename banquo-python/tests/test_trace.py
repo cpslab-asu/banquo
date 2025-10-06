be75e7ff0653dd3f06431b63345b018540cbcf69
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-10-06 10:50:00 -0700

    Create test module for trace datatype

---
+++ banquo-python/tests/test_trace.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from banquo import Trace
+
+def test_trace():
+    elements = {
+        0.0: {"x": 0.0, "y": 1.0},
+        1.0: {"x": 7.2, "y": 1.1},
+        2.0: {"x": 13.3, "y": 1.2},
+        3.0: {"x": 21.4, "y": 1.3},
+        4.0: {"x": 30.1, "y": 1.2},
+        5.0: {"x": 38.2, "y": 1.5},
+    }
+
+    times = elements.keys()
+    states = elements.values()
+
+    assert Trace(elements) == Trace.from_timed_states(times, states)  # Ensure constructor parity
+    assert Trace(Trace(elements)) == Trace(elements)  # Ensure conversion is lossless
