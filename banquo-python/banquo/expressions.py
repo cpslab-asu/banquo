94ed1183f6544439a42a64b4f245ad4944e1a64e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-10-07 09:21:14 -0700

    Change Predicate base class name

--- banquo-python/banquo/expressions.py
+++ banquo-python/banquo/expressions.py
@@ -4,9 +4,9 @@ from collections.abc import Mapping
 
 from ._banquo_impl import Predicate as _Predicate
 from .operators import OperatorMixin
-from .core import TraceWrapper
+from .core import EnsureOutput
 
 
-class Predicate(TraceWrapper[dict[str, float], float], OperatorMixin):
+class Predicate(EnsureOutput[dict[str, float], float], OperatorMixin):
     def __init__(self, coefficients: Mapping[str, float], constant: float):
         super().__init__(_Predicate(dict(coefficients), constant))
