b6811afb233607c0663bd469204c8bef62062ec6
Quinn Thibeault <qthibeault@proton.me>
2026-06-24 20:45:20 -0700

    refactor(parser): Parse MTL bounds into ranges instead of tuples

--- banquo-parser/src/mtl.rs
+++ banquo-parser/src/mtl.rs
@@ -1,19 +1,22 @@
+use std::ops::RangeInclusive;
+
 use chumsky::prelude::*;
 
 use crate::ltl;
 use crate::num;
 
-pub fn bounds<'src>() -> impl Parser<'src, &'src str, (f64, f64)> + Clone {
+pub fn bounds<'src>() -> impl Parser<'src, &'src str, RangeInclusive<f64>> + Clone {
     num()
         .then_ignore(just(","))
         .then(num())
         .delimited_by(just("{"), just("}"))
+        .map(|(start, end)| start..=end)
 }
 
-pub fn always<'src>() -> impl Parser<'src, &'src str, Option<(f64, f64)>> + Clone {
-    ltl::always().ignore_then(bounds()).or_not()
+pub fn always<'src>() -> impl Parser<'src, &'src str, Option<RangeInclusive<f64>>> + Clone {
+    ltl::always().ignore_then(bounds().or_not())
 }
 
-pub fn eventually<'src>() -> impl Parser<'src, &'src str, Option<(f64, f64)>> + Clone {
-    ltl::eventually().ignore_then(bounds()).or_not()
+pub fn eventually<'src>() -> impl Parser<'src, &'src str, Option<RangeInclusive<f64>>> + Clone {
+    ltl::eventually().ignore_then(bounds().or_not())
 }
