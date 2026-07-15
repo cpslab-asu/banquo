cf255b7bf386c6e460df98f41dde07c8d7c8012c
Quinn Thibeault <qthibeault@proton.me>
2026-07-14 21:55:35 -0700

    feat(core): Export ForwardEvaluationError type from operators module

--- banquo-core/src/operators/mod.rs
+++ banquo-core/src/operators/mod.rs
@@ -149,7 +149,7 @@ mod first_order;
 mod forward;
 
 pub use first_order::{And, BinaryEvaluationError, BinaryOperatorError, Implies, Not, Or};
-pub use forward::{Always, Eventually, ForwardOperatorError, Interval, Next, Until};
+pub use forward::{Always, Eventually, ForwardEvaluationError, ForwardOperatorError, Interval, Next, Until};
 
 mod test {
     use thiserror::Error;
